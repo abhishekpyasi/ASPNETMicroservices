08ca3248f29e39dcff9150b2d2fbef289a4967a1
abhishekpyasi <pyasi.abhishek@gmail.com>
2022-11-11 14:56:19 +0530

    basket microservices

---
+++ src/Services/Basket/Basket.API/Entities/ShoppingCartItem.cs
@@ -0,0 +1,16 @@
+﻿namespace Basket.API.Entities
+{
+    public class ShoppingCartItem
+    {
+
+
+        public int Quantity;
+        public string Color;
+        public decimal Price;
+        public string ProductID;
+        public string ProductName;
+
+
+
+    }
+}
